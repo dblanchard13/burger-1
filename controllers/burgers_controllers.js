17654bccd2fb3d25df7db3b6ab5975f27bc3405e
Tony Tran <tcbtran@T-MacBook-Pro.local>
2017-05-05 16:46:57 -0700

    Save Point

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -6,3 +6,32 @@ var router = express.Router();
 var burger = require('../models/burger.js');
 
 // Create routes and logic within routes
+router.get("/", function(req, res) {
+	burger.all(function(data) {
+		var hbsObject = {
+			burgers: data
+		};
+		console.log(hbsObject);
+		res.render("index", hbsObject);
+	});
+});
+
+router.post("/", function(req, res) {
+	burger.insert(["burger_name", "devoured"], [req.body.name, req.body.devoured], function() {
+		res.redirect("/");
+	});
+});
+
+router.put("/:id", function(req, res) {
+	var condition = "id = " + req.params.id;
+
+	console.log("condition", condition);
+	burger.update({
+		devoured: req.body.devoured
+	}, condition, function() {
+		res.redirect("/");
+	});
+});
+
+// Export routes to be used in server.js
+module.exports = router;
--- server.js
+++ server.js
@@ -2,3 +2,25 @@
 var express = require('express');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
+
+var port = 3000;
+
+var app = express();
+
+// Serve static content from "public" directory within app folder
+app.use(express.static(process.cwd() + "/public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set Handlebars
+var exphbs = require('express-handlebars');
+
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+// Import our routes and give server access to them
+var routes = require("./controllers/burgers_controllers.js");
+
+app.use("/", routes);
+
+app.listen(port);
